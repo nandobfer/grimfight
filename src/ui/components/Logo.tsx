ca755ad11eadaabed967d84926b688ec2a38b7cb
nandobfer <nandobfer@yahoo.com.br>
2025-09-15 12:03:08 -0300

    assets url

--- src/ui/components/Logo.tsx
+++ src/ui/components/Logo.tsx
@@ -8,6 +8,10 @@ interface LogoProps {
 export const Logo:React.FC<LogoProps> = (props) => {
     
     return (
-        <Avatar variant='square' src='/grimfight.png' sx={{width: 600, height: 'auto', marginTop: -17, marginBottom: -20, marginLeft: -2, marginRight: -2}} />
+        <Avatar
+            variant="square"
+            src="grimfight.png"
+            sx={{ width: 600, height: "auto", marginTop: -17, marginBottom: -20, marginLeft: -2, marginRight: -2 }}
+        />
     )
 }
