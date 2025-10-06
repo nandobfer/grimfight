3eefff8d015b2fb7070fd0c4215c64f710f557cf
nandobfer <nandobfer@yahoo.com.br>
2025-10-06 17:24:50 -0300

    game records itens

--- src/ui/GameMenu/RecordHistory/RecordItem.tsx
+++ src/ui/GameMenu/RecordHistory/RecordItem.tsx
@@ -6,6 +6,8 @@ import { colorFromLevel, convertColorToString } from "../../../game/tools/Rarity
 import { TraitsRegistry } from "../../../game/systems/Traits/TraitsRegistry"
 import { TraitList } from "../../Traits/TraitList"
 import { AbilityTooltip } from "../../CharacterSheet/AbilityTooltip"
+import { CharacterItems } from "../../CharacterSheet/CharacterItems"
+import { ItemIcon } from "../../components/ItemIcon"
 
 interface RecordItemProps {
     record: GameRecord
@@ -32,44 +34,48 @@ export const RecordItem: React.FC<RecordItemProps> = (props) => {
 
     return (
         <>
+            <Box sx={{ flexDirection: "column" }}>
+                <Typography variant="caption">{new Date(props.record.finishedAt || Date.now()).toLocaleDateString("pt-br")}</Typography>
+                <Tooltip title={"Andar alcançado"}>
+                    <Box sx={{ gap: 1, alignItems: "center" }}>
+                        <Avatar src={"assets/darkest_skull.webp"} sx={{ margin: -1 }} />
+                        <Typography variant="subtitle2" sx={{ fontWeight: "bold" }}>
+                            {props.record.floor}
+                        </Typography>
+                    </Box>
+                </Tooltip>
+            </Box>
+            <TraitList traits={traits} row />
             <Box sx={{ gap: 1, alignItems: "center" }}>
-                <Box sx={{ flexDirection: "column" }}>
-                    <Typography variant="caption">{new Date(props.record.finishedAt || Date.now()).toLocaleDateString("pt-br")}</Typography>
-                    <Tooltip title={"Andar alcançado"}>
-                        <Box sx={{ gap: 1, alignItems: "center" }}>
-                            <Avatar src={"assets/darkest_skull.webp"} sx={{ margin: -1 }} />
-                            <Typography variant="subtitle2" sx={{ fontWeight: "bold" }}>
-                                {props.record.floor}
-                            </Typography>
-                        </Box>
-                    </Tooltip>
-                </Box>
-
-                <Box sx={{ gap: 0.5 }}>
+                <Box sx={{ gap: 1 }}>
                     {props.record.comp.map((char) => (
-                        <AbilityTooltip description={char.abilityDescription} placement="auto">
-                            <Badge
-                                key={char.id}
-                                badgeContent={`${char.level}`}
-                                slotProps={{
-                                    badge: {
-                                        sx: {
-                                            bgcolor: convertColorToString(colorFromLevel(char.level)),
-                                            color: "background.default",
-                                            fontWeight: "bold",
-                                            fontSize: isMobile ? 8 : undefined,
+                        <Box key={char.id} sx={{ position: "relative" }}>
+                            <AbilityTooltip description={char.abilityDescription} placement="auto">
+                                <Badge
+                                    key={char.id}
+                                    badgeContent={`${char.level}`}
+                                    slotProps={{
+                                        badge: {
+                                            sx: {
+                                                bgcolor: convertColorToString(colorFromLevel(char.level)),
+                                                color: "background.default",
+                                                fontWeight: "bold",
+                                                fontSize: isMobile ? 8 : undefined,
+                                            },
                                         },
-                                    },
-                                }}
-                            >
-                                <CharacterAvatar name={char.name} size={30} variant="square" />
-                            </Badge>
-                        </AbilityTooltip>
-                    ))}
-                </Box>
+                                    }}
+                                >
+                                    <CharacterAvatar name={char.name} size={50} variant="circular" />
+                                </Badge>
+                            </AbilityTooltip>
 
-                <Box sx={{ marginLeft: 1 }}>
-                    <TraitList traits={traits} row />
+                            <Box sx={{ position: "absolute", bottom: -10, left: 0, gap: 0.5 }}>
+                                {char.items.map((item, index) => (
+                                    <ItemIcon key={item + index} itemKey={item} size={15} />
+                                ))}
+                            </Box>
+                        </Box>
+                    ))}
                 </Box>
             </Box>
             <Divider sx={{ width: 1 }} />
